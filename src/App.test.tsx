fb3fdc990ea74ad26d43c9aeb3c9da577b5767ea
Zane Starr <zcstarr@gmail.com>
2021-06-15 19:35:10 -0700

    fix: broken homepage test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { CssBaseline } from './styles';
+import { lightTheme } from './theme';
+import GlobalStyle from './styles/global';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument();
+  const WrappedApp = () => (<ThemeProvider theme={lightTheme}>
+    <CssBaseline />
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>)
+  render(<WrappedApp />);
+  const titleElement = screen.getByText(/^SHIPS$/)
+  expect(titleElement).toBeInTheDocument();
 });
