4e5e831277e780737ce27828531d687421946075
Zane Starr <zcstarr@gmail.com>
2021-07-31 15:52:11 -0700

    fix: basic default style

--- src/components/CommonButton.tsx
+++ src/components/CommonButton.tsx
@@ -4,30 +4,48 @@ import { ThemeProp } from "./types";
 
 const CommonButton = styled(BaseStyledButton)`
 background-color: ${({theme}: ThemeProp)=>theme.colors.button};
-font-size: 2.0rem;
+font-size: 1.3rem;
+font-weight: 900;
 width: 220px;
 height: 100px;
-border: 4px;
-border-radius: 4px;
+border-radius: 16px;
 border-style: solid;
 :hover {
   opacity: 0.6;
 }
+border-radius: 16px;
+border-style: solid;
+border-color: ${({theme}: ThemeProp)=>theme.colors.black}
+color: ${({theme}: ThemeProp)=>theme.colors.black};
+border-width: 4px;
+
+${({theme}: ThemeProp)=>theme.base.mediaQueries.sm} {
+border-width: 5px;
+}
 `
 export const CommonRoundedButton = styled(BaseStyledButton)`
 background-color: ${({theme}: ThemeProp)=>theme.colors.button};
-font-size: 2.0rem;
+color: ${({theme}: ThemeProp)=>theme.colors.black};
+font-size: 1.3rem;
 width: 220px;
 height: 100px;
-border: 4px;
 border-radius: 20px;
 border-style: solid;
+border-width: 4px;
+${({theme}: ThemeProp)=>theme.base.mediaQueries.sm} {
+border-width: 5px;
+}
 `
 export const CommonExtraRoundButton = styled(CommonRoundedButton)`
 border-radius: 40px;
 border-color: ${({theme}: ThemeProp)=>theme.colors.primary};
 :hover {
   opacity: 0.6;
+}
+border-width: 4px;
+color: ${({theme}: ThemeProp)=>theme.colors.black};
+${({theme}: ThemeProp)=>theme.base.mediaQueries.sm} {
+border-width: 5px;
 }
 `
 export default CommonButton;
