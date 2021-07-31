c8469bc532b5d92bc2f6772bc060e79a85bdb993
Zane Starr <zcstarr@gmail.com>
2021-07-31 15:52:36 -0700

    fix: basic default styling

--- src/components/DividerWithText.tsx
+++ src/components/DividerWithText.tsx
@@ -5,15 +5,18 @@ import { ThemeProp } from "./types";
 const DividerWithText = styled.div`
   display: table;
   line-height: 20px;
+  margin-top: 58px;
   letter-spacing: 2px;
-  color: (0,0,0);
+  font-weight: 900;
+  font-size: 1.3rem;
+  border-color: ${({theme}: ThemeProp)=>theme.colors.black};
   text-align: center;
   width: 90%;
-  margin: 0 auto;
+  margin: 40px auto;
   ::before , ::after {
     position: relative;
     top: 0.6em;
-    border-top: 1px solid black;
+    border-top: 1px solid ${({theme}: ThemeProp)=>theme.colors.black};;
     content: "";
     display: table-cell;
     width: 40%;
