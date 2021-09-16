ffc49b734609d981843e7eb397c8e5e3452cb00e
Zane Starr <zcstarr@gmail.com>
2021-09-16 11:20:21 -0700

    chore: readjust button sizes

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -1,17 +1,17 @@
 import styled from "styled-components";
-import BaseStyledButton from "./Button";
+import {CommonButton} from "./CommonButton";
 import { ThemeProp } from "./types";
 
 
-const JoinButton = styled(BaseStyledButton)`
+const JoinButton = styled(CommonButton)`
 background-color: ${({theme}: ThemeProp)=>theme.colors.button};
-font-size: 2.5rem;
+font-size: 2.0rem;
+font-weight: 550;
 width: 220px;
 height: 65px;
 ${({ theme }: ThemeProp) => theme.base.mediaQueries.md} {
-  font-size: 3.5rem;
-  width: 292px;
-  height: 89px;
+  width: 250px;
+  height: 80px;
 }
 `
 export default JoinButton;
