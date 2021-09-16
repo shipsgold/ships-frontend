b2190e1080779cae4a3733ab4d7729f73d5cadd6
Zane Starr <zcstarr@gmail.com>
2021-09-16 11:23:22 -0700

    chore: add non black default color for body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
   body {
+    color: #333333;
     background: white;
   }
 `
