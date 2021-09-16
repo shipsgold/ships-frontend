102c70447d5d9fb3397c388771ffc8fc09701053
Zane Starr <zcstarr@gmail.com>
2021-09-16 11:23:48 -0700

    chore: lint

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as Landing } from "./Landing";
 export {default as Signup} from "./Signup";
 export {default as Login} from "./Login";
+export {default as SignupThankyou} from './SignupThankyou';
