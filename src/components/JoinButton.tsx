ce6f5879e7f87b65a665a1b55cce8163dc75eca9
Zane Starr <zcstarr@gmail.com>
2021-10-13 16:47:20 -0700

    fix: refactor join to link to sign up form

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -3,11 +3,22 @@ import {CommonButton} from "./CommonButton";
 import { ThemeProp } from "./types";
 
 
-const JoinButton = styled(CommonButton)`
+const JoinButton = styled(CommonButton).attrs(
+  props => ({
+    as: "a",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLSeg4C9rmJlzs0u4D4dYx3_y5ouWFNkczw1pjkIZUIgV7ifvfg/viewform"
+  })
+)`
 background-color: ${({theme}: ThemeProp)=>theme.colors.button};
 font-size: 2.0rem;
 font-weight: 550;
 width: 220px;
+text-decoration: none;
+text-align: center;
+display: flex;
+flex-direction: column;
+align-self: center;
+justify-content: center;
 height: 65px;
 ${({ theme }: ThemeProp) => theme.base.mediaQueries.md} {
   width: 250px;
